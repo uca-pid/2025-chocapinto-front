5adf8d969a4e673dc966925025871ffa2c07654e
WhiteGori <juanmblancog@gmail.com>
2025-11-05 20:53:10 -0300

    Revert "cambio de api"
    This reverts commit 78e1d7d6772cc5e4c51dfa8960f9bede799e84ca.

--- js/app.js
+++ js/app.js
@@ -24,7 +24,7 @@ import { API_URL } from "./env.js";
             }
 
             // Redirigir al main
-            window.location.href = "/html/main.html";
+            window.location.href = "main.html";
         } else {
             document.getElementById("errorMsg").style.display = "block";
         }
